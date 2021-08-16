8dd9f995e3f6234b979c4090bcd08b35b23badae
pharreX19 <pharrex19@gmail.com>
2021-08-16 08:04:40 +0500

    fix class name

--- database/migrations/2021_07_29_013427_create_permission_role_table.php
+++ database/migrations/2021_07_29_013427_create_permission_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePermissionRolesTable extends Migration
+class CreatePermissionRoleTable extends Migration
 {
     /**
      * Run the migrations.
