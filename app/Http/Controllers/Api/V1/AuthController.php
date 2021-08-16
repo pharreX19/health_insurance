962f647937e365482be5b8beaea50933e188bb5e
pharreX19 <pharrex19@gmail.com>
2021-08-16 07:57:00 +0500

    set selected service provider on session

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Api\V1\AbstractController;
 use Illuminate\Http\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends AbstractController
 {
@@ -13,19 +14,34 @@ class AuthController extends AbstractController
     {
         $credentials = $request->only(['email', 'password']);
         if(!$token = Auth::attempt($credentials)){
-            return $this->respondError(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+            return $this->respondError(['error' => 'Incorrect email or password'], Response::HTTP_UNAUTHORIZED);
         }
         // $permissions = Auth::user()->role->permissions->pluck('slug')->toArray();
-        $user = Auth::user()->load('role.permissions');
-        if($user->user_type === 'App\Receptionist'){
-            dd(app('App\Models\Receptionist'));
-            dd($user->user_type->serviceProvider());
+        
+        if(auth()->user()->serviceProviders()->count() > 1 && !$request['service_provider_id']){
+            return $this->respondError([
+                'error' => 'No service provider found',
+                'service_providers' => auth()->user()->serviceProviders->toArray()
+            ], Response::HTTP_CONFLICT);
         }
+        
+        if(auth()->user()->role_id != 1){
+            $_SESSION['serviceProviderId'] = $request['service_provider_id'] ?? auth()->user()->serviceProviders()->first()->value('id');
+        }
+
+
+        // $user = auth()->claims(['foo'=> 'baz'])->attempt($credentials);
+        // dd($user);
+        $user = Auth::user()->load('role.permissions');
+        
         return $this->respondWithToken($token, $user);
     }
 
     public function logout(Request $request)
     {
+        if(isset($_SESSION['serviceProviderId'])){
+            unset($_SESSION['serviceProviderId']);
+        }
         Auth::logout();
         return $this->respondSuccess(null, 'Successfully logged out', Response::HTTP_OK);
     }
@@ -40,6 +56,7 @@ class AuthController extends AbstractController
             'access_token' => $token,
             'token_type' => 'bearer',
             'user' => $user,
+            'service_provider' => $_SESSION['serviceProviderId'] ?? null,
             'expires_in' => Auth:: factory()->getTTL() * 60
         ], 'Successfully logged in', Response::HTTP_OK);
     }
