5e9c4e4e78abf8d8a0998c335fe152ede9171b6e
pharreX19 <pharrex19@gmail.com>
2021-08-20 15:06:30 +0500

    changed cors allowed origin header

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', 'http://localhost:8080')
+        ->header('Access-Control-Allow-Origin', 'http://ec2-54-179-124-154.ap-southeast-1.compute.amazonaws.com')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization')
         ->header('Access-Control-Allow-Credentials', 'true');
