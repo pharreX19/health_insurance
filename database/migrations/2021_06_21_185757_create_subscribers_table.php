143a155237fbeb0208a545d448c969b2290fb565
pharreX19 <pharrex19@gmail.com>
2021-09-27 06:49:48 +0500

    removed policy number

--- database/migrations/2021_06_21_185757_create_subscribers_table.php
+++ database/migrations/2021_06_21_185757_create_subscribers_table.php
@@ -16,13 +16,13 @@ class CreateSubscribersTable extends Migration
     {
         Schema::create('subscribers', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 255)->unique();
+            $table->string('name', 255);
             $table->string('passport')->nullable()->unique();
             $table->string('national_id')->nullable()->unique()->index();
             $table->string("work_permit")->nullable()->index()->unique();
             $table->string('country')->default('Maldives');
             $table->string('contact')->nullable();
-            $table->string('policy_number');
+            // $table->string('policy_number');
             $table->foreignId('company_id')->nullable()->constrained()->onDelete("set null");
             $table->foreignId('plan_id')->nullable()->constrained()->onDelete("set null");
             $table->softDeletes();
