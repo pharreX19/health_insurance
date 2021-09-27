2279b46ac9040691fffc1324f276e0f594ca15e3
pharreX19 <pharrex19@gmail.com>
2021-09-27 06:43:59 +0500

    set allowed hots from env file

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', 'http://localhost:8080')
+        ->header('Access-Control-Allow-Origin', env('VUE_APP_URL'))
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization')
         ->header('Access-Control-Allow-Credentials', 'true');
